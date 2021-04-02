778356539273951d2820f9f357674c4d8ca2268a
Jiri Srba <srba.jiri@operatorict.cz>
2021-04-02 10:29:51 +0200

    docs: update dataswitch mode

--- huawei-lte.py
+++ huawei-lte.py
@@ -19,7 +19,11 @@ from huawei_lte_api.AuthorizedConnection import AuthorizedConnection
 
 
 def main(argv):
-  """main"""
+  """
+  toggle LTE modem state
+
+  :param dataswitch: 0 to disable LTE modem, 1 to enable LTE modem
+  """
 
   admin_password = os.environ.get("ADMIN_PASSWORD")
   connection = AuthorizedConnection('http://192.168.8.1/',
@@ -30,12 +34,13 @@ def main(argv):
 
   if len(argv) == 2:
     if argv[1] == "enable":
-      client.dial_up.set_mobile_dataswitch(0)
-    elif argv[1] == "disable":
       client.dial_up.set_mobile_dataswitch(1)
+    elif argv[1] == "disable":
+      client.dial_up.set_mobile_dataswitch(0)
     else:
       sys.exit("ERROR: unknown parameter")
   else:
+    # print status
     status = client.dial_up.mobile_dataswitch()
     if int(status['dataswitch']) == 0:
       print('status: disabled')
